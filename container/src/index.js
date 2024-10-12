06e65977e3e33cb6fdc0829a2d11b2a66f29e482
DarkSJ1998 <sarthakjn12@gmail.com>
2024-10-12 20:09:02 +0530

    Added ModuleFederationPlugin to Container application to utilize remote code

--- container/src/index.js
+++ container/src/index.js
@@ -1,1 +1,6 @@
-console.log('Container works!');
+/**
+ * This is needed to give Webpack some time
+ * and a chance to fetch the required JavaScript
+ * files, prior to mounting the "bootstrap.js"
+ */
+import('./bootstrap');
